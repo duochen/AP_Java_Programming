c2d40df5111c6e7f47d2ecd5afebb7a3a61b3cb2
duochen <duochen@yahoo.com>
2025-06-04 15:41:27 -0500

    check in code

--- IdeaProjects/JavaProject/src/Main.java
+++ IdeaProjects/JavaProject/src/Main.java
@@ -2,14 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int number = 25;
+        int i;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        boolean isPrime = true;
+        for (i = 2; i < number && isPrime; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+            }
         }
+        System.out.println("i is " + i + " isPrime is " + isPrime);
     }
 }
